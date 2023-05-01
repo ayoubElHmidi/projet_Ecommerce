289b237fa6855cbe49d262a753ba4ee086e76556
mohammed123 <simohamaq@gmail.com>
2023-05-01 01:41:50 +0100

    tst tst 2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+Route::get('/homme', function () {return view('home');});
